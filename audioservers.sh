89b9f8e9a69347cfae74ed976f220bbfe3258f6c
Chan Jun Shern <chanjunshern@gmail.com>
2018-01-02 17:05:16 +0000

    Add Composer class, handle input with callbacks

--- audioservers.sh
+++ audioservers.sh
@@ -7,14 +7,12 @@ case $1 in
 		aplay -l | grep card
 		read -p "> " hw
 		echo "You selected HW:$hw"
-
 		# Start JACK
 		echo
 		echo "Starting JACK..."
 		pasuspender --\
 			jackd -d alsa --device hw:$hw --rate 44100 --period 128 --softmode \
 			&>/tmp/jackd.out &
-
 		# Start fluidsynth
 		echo
 		echo "Starting Fluidsynth..."
@@ -23,22 +21,21 @@ case $1 in
 			/usr/share/sounds/sf2/FluidR3_GM.sf2 \
 			&>/tmp/fluidsynth.out &
 		sleep 1
-
 		echo
 		if pgrep jackd && pgrep fluidsynth
 		then	echo "Audio servers running"
 		else 
 			echo "There was a problem starting the audio servers."
 		fi
-
 		;;
+
 	stop )
 		killall fluidsynth
 		killall jackd
 		echo "Thank you for the music."
 		;;
+
 	* )
 		echo "Please specify start or stop..."
 		;;
 esac
-
--- main.py
+++ main.py
@@ -1,13 +1,43 @@
 import mido
+import random
+import collections
+import time
+
+MEMORY_LENGTH = 50
+COMP_CHANNEL = 1
+
+class Composer:
+    """Composer class stores causal state of music, and generates new notes."""
+
+    def __init__(self):
+        self.player_notes = collections.deque(maxlen=MEMORY_LENGTH)
+        self.own_notes = collections.deque(maxlen=MEMORY_LENGTH)
+        # Prep the composer
+        inst_change = mido.Message('program_change', channel=COMP_CHANNEL, program=random.randint(1, 127))
+        outport.send(inst_change)
+
+    def message_callback(self, msg):
+        outport.send(msg) # Direct bypass
+        self.add_to_player_memory(msg)
+
+    def add_to_player_memory(self, msg):
+        self.player_notes.append(msg)
+
+    def add_to_own_memory(self, msg):
+        self.own_notes.append(msg)
+
+    def generate_comp(self):
+        return
+        # if len(self.player_notes):
+        #     newmsg = self.player_notes[random.randint(0, len(self.player_notes)-1)]
+        #     outport.send(newmsg)
+        # newmsg = mido.Message(msg.type, channel=COMP_CHANNEL, note=msg.note+4, velocity=msg.velocity)
+        # return newmsg.copy(channel=COMP_CHANNEL)
 
 if __name__ == "__main__":
-    # Look for input ports
-    # print mido.get_input_names()
-    # print mido.get_output_names()
-    # IN_PORT = mido.get_input_names()[0]
-    # OUT_PORT = mido.get_output_names()[0]
-    with mido.open_input('mido_in', virtual=True) as inport, \
-        mido.open_output('mido_out', virtual=True, autoreset=True) as outport:
-        # while (1):
-        for msg in inport: # Loops until port is closed
-            outport.send(msg)
+    comp = Composer()
+    with mido.open_output('mido_out', virtual=True, autoreset=True) as outport, \
+        mido.open_input('mido_in', virtual=True, callback=comp.message_callback) as inport:
+        while 1:
+            time.sleep(25)
+            comp.generate_comp()
