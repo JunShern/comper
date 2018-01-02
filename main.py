a06d310514dbb8115fd0f56ec25a0cde58655588
Chan Jun Shern <chanjunshern@gmail.com>
2018-01-02 22:49:46 +0000

    Add RandomMemory comper, fix channel number bug

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ import collections
 import time
 
 MEMORY_LENGTH = 50
-COMP_CHANNEL = 1
+COMP_CHANNEL = 2 # MIDI channel number (1-16)
 
 class Composer:
-    """Composer class stores causal state of music, and generates new notes."""
-
+    """
+    Base class for possible composer types, implements:
+    - causal memory states of music
+    - callback function for new notes
+    - empty comping function
+    """
     def __init__(self):
         self.player_notes = collections.deque(maxlen=MEMORY_LENGTH)
         self.own_notes = collections.deque(maxlen=MEMORY_LENGTH)
-        # Prep the composer
-        inst_change = mido.Message('program_change', channel=COMP_CHANNEL, program=random.randint(1, 127))
-        outport.send(inst_change)
 
     def message_callback(self, msg):
+        print(msg)
         outport.send(msg) # Direct bypass
-        self.add_to_player_memory(msg)
+        if msg.type == "note_on" or msg.type == "note_off":
+            self.add_to_player_memory(msg)
 
     def add_to_player_memory(self, msg):
         self.player_notes.append(msg)
@@ -28,16 +31,28 @@ class Composer:
 
     def generate_comp(self):
         return
-        # if len(self.player_notes):
-        #     newmsg = self.player_notes[random.randint(0, len(self.player_notes)-1)]
-        #     outport.send(newmsg)
-        # newmsg = mido.Message(msg.type, channel=COMP_CHANNEL, note=msg.note+4, velocity=msg.velocity)
-        # return newmsg.copy(channel=COMP_CHANNEL)
+
+class RandomMemory(Composer):
+    """
+    Simply plays back notes at random from memory of player_notes
+    """
+    def __init__(self):
+        Composer.__init__(self)
+
+    def generate_comp(self):
+        if len(self.player_notes):
+            newmsg = self.player_notes[random.randint(0, len(self.player_notes)-1)]
+            newmsg = newmsg.copy(channel=COMP_CHANNEL-1) # Mido channels from 0-15, MIDI 1-16
+            print(newmsg)
+            outport.send(newmsg)
 
 if __name__ == "__main__":
-    comp = Composer()
+    comp = RandomMemory()
     with mido.open_output('mido_out', virtual=True, autoreset=True) as outport, \
         mido.open_input('mido_in', virtual=True, callback=comp.message_callback) as inport:
+        # Prep the composer
+        # inst_change = mido.Message('program_change', channel=COMP_CHANNEL, program=random.randint(1, 127))
+        # outport.send(inst_change)
         while 1:
-            time.sleep(25)
+            time.sleep(.2)
             comp.generate_comp()
