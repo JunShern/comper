d31a4f585ffae2643a47424ca4f5298b7d817057
Chan Jun Shern <chanjunshern@gmail.com>
2018-01-07 22:22:00 +0000

    Add deltatime for note durations, update Random comp

--- composers.py
+++ composers.py
@@ -3,7 +3,7 @@ import random
 import collections
 import mido
 
-MEMORY_LENGTH = 50
+MEMORY_LENGTH = 500
 COMP_CHANNEL = 2 # MIDI channel number (1-16)
 
 class Composer(object):
@@ -17,11 +17,20 @@ class Composer(object):
         self.player_notes = collections.deque(maxlen=MEMORY_LENGTH)
         self.own_notes = collections.deque(maxlen=MEMORY_LENGTH)
         self.active_notes = []
+        self.previous_event_time = 0
 
     def register_player_note(self, msg):
         """
         Keep track of all messages, and which notes are active
         """
+        # Delta time gives the time since the previous MIDI event
+        deltatime = 0.5 # Arbitrarily set time for first note
+        timenow = time.time()
+        if self.previous_event_time:
+            deltatime = timenow - self.previous_event_time
+        self.previous_event_time = timenow
+        msg.time = deltatime
+
         if msg.type == "note_on":
             if not msg.note in self.active_notes:
                 self.active_notes.append(msg.note)
@@ -49,8 +58,10 @@ class RandomMemory(Composer):
         if len(self.player_notes):
             msg = self.player_notes[random.randint(0, len(self.player_notes)-1)]
             msg = msg.copy(channel=COMP_CHANNEL-1) # Mido channels from 0-15, MIDI 1-16
+            time.sleep(msg.time)
             outport.send(msg)
-        time.sleep(.2)
+        else:
+            time.sleep(0.2)
 
 class Arpeggiator(Composer):
     """
@@ -81,6 +92,14 @@ class MarkovMonophonicDurationless(Composer):
         Keep track of all messages, and which notes are active.
         Also, update Markov Chain.
         """
+        # Delta time gives the time since the previous MIDI event
+        deltatime = 0
+        timenow = time.clock()
+        if self.previous_event_time:
+            deltatime = timenow - self.previous_event_time
+        self.previous_event_time = timenow
+        msg.time = deltatime
+
         if msg.type == "note_on":
             if not msg.note in self.active_notes:
                 self.active_notes.append(msg.note)
