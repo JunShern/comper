6225152644f3fe52418f9ae3c618fa0171c2c357
Chan Jun Shern <chanjunshern@gmail.com>
2018-01-13 00:51:30 +0000

    Clean midi file, add argv to filereaders, tweak histogram

--- readMIDIfile.py
+++ readMIDIfile.py
@@ -1,9 +1,21 @@
 import time
 import mido
+import sys
 
 if __name__ == "__main__":
-    with mido.open_output('midi_read_out', virtual=True, autoreset=True) as outport:
-        for msg in mido.MidiFile('midi_files/abba-winner_takes_it_all.mid'):
-            time.sleep(msg.time)
-            if not msg.is_meta:
-                outport.send(msg)
+    if len(sys.argv) == 2:
+        with mido.open_output('midi_read_out', virtual=True, autoreset=True) as outport:
+            midifile = mido.MidiFile(sys.argv[1])
+            print "Number of tracks: " + str(len(midifile.tracks))
+            for i, track in enumerate(midifile.tracks):
+                print "(Track " + str(i) + "): " + str(track.name) + " has " + str(len(track)) + " messages."
+            if midifile.type != 2:
+                for msg in midifile:
+                    print msg
+                    time.sleep(msg.time)
+                    if not msg.is_meta:
+                        outport.send(msg)
+            else:
+                print "Asynchronous midi file not supported, exiting..."
+    else:
+        print "Incorrect number of arguments."
--- trainKeyDetection.py
+++ trainKeyDetection.py
@@ -1,6 +1,7 @@
 """
 Key detector based on bag-of-words histogram detection
 """
+import sys
 import time
 import mido
 import operator
@@ -11,6 +12,8 @@ NOTE_NAME = {0:"C", 1:"C#", 2:"D", 3:"D#", 4:"E", 5:"F", \
 class KeyDetector(object):
     def __init__(self):
         self.histogram = {}
+        for keyname in NOTE_NAME.values():
+            self.histogram[keyname] = 0
 
     def midi_to_note(self, note_number):
         normalized_note_number = note_number % 12
@@ -27,15 +30,15 @@ class KeyDetector(object):
             if not msg.is_meta:
                 if msg.type == 'note_on':
                     keyname = self.midi_to_note(msg.note)
-                    if keyname in self.histogram:
-                        self.histogram[keyname] = self.histogram[keyname] + 1
-                    else:
-                        self.histogram[keyname] = 1
+                    self.histogram[keyname] = self.histogram[keyname] + 1
         return self.histogram
 
 if __name__ == "__main__":
-    key_detector = KeyDetector()
-    hist = key_detector.train_detector('midi_files/abba-winner_takes_it_all.mid')
-    print hist
-    hist = key_detector.normalize(hist)
-    print hist
+    if len(sys.argv) == 2:
+        key_detector = KeyDetector()
+        hist = key_detector.train_detector(sys.argv[1])
+        print hist
+        hist = key_detector.normalize(hist)
+        print hist
+    else:
+        print "Incorrect number of arguments."
